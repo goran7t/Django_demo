90c47d5649e36d13d355ee003b2369e27c9def17
goran7t <goran7t@yahoo.com>
2017-12-08 12:35:12 +0100

    Skeleton

--- demo/urls.py
+++ demo/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from demoapp.views import index, about, contact_us, clear
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',index, name='index'),
+    url(r'^about/$',about, name='about'),
+    url(r'^contact/$',contact_us, name='contact'),
+    url(r'^clear/$',clear, name='clear'),
 ]
